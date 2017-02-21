c57a6cc2557e4f364375acf1e492a9ae93d8b039
cloud-elric <humberto@2gom.com.mx>
2017-02-21 12:06:28 -0600

    peleando todavia con el gitignore

---
+++ protected/config/database.php
@@ -0,0 +1,30 @@
+<?php
+$debug = true;
+$dataBase = array ();
+if ($debug) {
+	$dataBase = array (
+				
+			'connectionString' => 'mysql:host=192.168.0.6;dbname=comite_2017',
+			'emulatePrepare' => true,
+			'username' => 'root',
+			'password' => 'root',
+			//'charset' => 'utf8',
+				
+			'schemaCachingDuration'=>3600,
+	);
+} else {
+	$dataBase = array (
+				
+		'connectionString' => 'mysql:host=mysql3000.mochahost.com;dbname=beto2gom_comite_canada_test',
+		'emulatePrepare' => true,
+		'username' => 'beto2gom_cocate',
+		'password' => 'c0d1ngG33k',
+		'charset' => 'utf8',
+		'tablePrefix' => 'tbl_',
+		
+		'schemaCachingDuration'=>3600,
+		
+	)
+	;
+}
+return $dataBase;
