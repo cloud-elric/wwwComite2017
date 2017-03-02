ce9f244601093bd13a4e83e6216bc1ee9a044f17
Raul Alanis <rulosc07@gmail.com>
2017-03-02 13:07:40 -0600

    correccion de errores al resetear la password

--- protected/views/site/resetPassword.php
+++ protected/views/site/resetPassword.php
@@ -14,26 +14,26 @@ $this->pageTitle = Yii::t('general', 'cambiarPassTitle');
 	<div class="row">
 
 		<!-- .col -->
-		<div class="login-col-flex col-sm-6 col-md-6">
-			<div class="login-text">
-				<h2>
-					<?=Yii::t('general', 'bienvenido')?> <img
-						src="<?php echo Yii::app()->request->baseUrl; ?>/images/hardcode/Contest-Logo.png"
-						alt="<?=$concurso->txt_name?>">
-				</h2>
+<!-- 		<div class="login-col-flex col-sm-6 col-md-6"> -->
+<!-- 			<div class="login-text"> -->
+<!-- 				<h2> -->
+					<?php //echo Yii::t('general', 'bienvenido')?> <!-- <img
+						src="<?php //echo Yii::app()->request->baseUrl; ?>/images/hardcode/Contest-Logo.png"
+						alt="<?php //echo$concurso->txt_name?>"> -->
+<!-- 				</h2> -->
 				<!-- <button type="button" class="btn btn-blue">Consulta las bases del concurso</button> -->
-				<a href="<?=$concurso->txt_url_contest?>" target="_blank"
-					class="btn btn-blue"><?=Yii::t('general', 'consulta')?></a>
-			</div>
-		</div>
+				<!-- <a href="<?php //echo $concurso->txt_url_contest?>" target="_blank"
+					class="btn btn-blue"><?php //echo Yii::t('general', 'consulta')?></a> -->
+<!-- 			</div> -->
+<!-- 		</div> -->
 		<!-- end / .col -->
 
 		<!-- .col -->
-		<div class="login-col-flex col-sm-6 col-md-6">
+		<div class="login-col-flex col-sm-6 col-md-6 col-md-offset-3">
 			<!-- .login-form -->
 			<div class="login-form">
 
-				<img src="<?= Yii::app()->request->baseUrl ?>/images/hardcode/WPC-Logo.png"
+				<img src="<?= Yii::app()->request->baseUrl ?>/images/hardcode/ms-icon-150x150.png"
 					alt="CFM">
 				
 
@@ -52,9 +52,9 @@ $this->pageTitle = Yii::t('general', 'cambiarPassTitle');
 					<p><?=Yii::t('resetPassword', 'instrucciones')?></p>
 
 
-					<?php echo $form->passwordField($model,'txt_password',array('size'=>10,'maxlength'=>10,"class"=>"form-control",'placeholder'=>Yii::t('resetPassword', 'placeholderPass'))); ?>
+					<?php echo $form->passwordField($model,'txt_password',array('size'=>10,'maxlength'=>10,"class"=>"form-control",'placeholder'=>Yii::t('resetPassword', 'Agregar contraseña'))); ?>
 
-					<?php echo $form->passwordField($model,'repetirPassword',array("class"=>"form-control",'placeholder'=>Yii::t('resetPassword', 'placeholderPassRepeat'))); ?>
+					<?php echo $form->passwordField($model,'repetirPassword',array("class"=>"form-control",'placeholder'=>Yii::t('resetPassword', 'Repetir contraseña'))); ?>
 
 					<?php # echo $form->error($model,'username'); ?>
 					
@@ -66,7 +66,7 @@ $this->pageTitle = Yii::t('general', 'cambiarPassTitle');
 
 					<?php echo CHtml::submitButton(Yii::t('resetPassword', 'submit'), array("class"=>"btn btn-blue")); ?>
 
-					<?php echo CHtml::link(Yii::t('login', 'necesitarCuenta'), array("usrUsuarios/registrar", "t"=>$t), array("class"=>"necesito")); ?>
+					<?php echo CHtml::link(Yii::t('login', 'necesitarCuenta'), array("usrUsuarios/registrar"), array("class"=>"necesito")); ?>
 
 				<?php $this->endWidget(); ?>
 
