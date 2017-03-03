b7f2785a25db063a720893613087225274d8bb68
Raul Alanis <rulosc07@gmail.com>
2017-03-03 12:57:46 -0600

    combios de lenguaje en correo recuperar contraseña

--- protected/messages/es_es/recoveryPassword.php
+++ protected/messages/es_es/recoveryPassword.php
@@ -1,1 +1,10 @@
 <?php
+return array(
+		'gretting'=>'Olvidaste tu contraseña?',
+		'descriptionMessage1'=>'We just got a request telling us that this account needs a password reset, to reset this password please click on the following ',
+		'descriptionMessage1'=>'Recibimos la peticion de que esta cuenta necesita un cambio de contraseña, para cambiar la contraseña porfavor hacer click en lo siguiente',
+		'linkText'=>'link:',
+		'btnText'=>'Cambiar contraseña',
+		'descriptionMessage2'=>'Si tu no quieres cambiar tu contraseña, porfavor no tomar en cuenta este correo, este link expirara en 24 horas',
+		'subject'=>'Cambiar contraseña'
+);
